2875f76364dc184f3992227b2d7a1bb8ec38d4ba
kimnorth <benignhumour@gmail.com>
2017-04-12 21:35:21 +0100

    Created a request to the spotify API and can console log the object.

--- public/app.js
+++ public/app.js
@@ -1,5 +1,21 @@
-var app = function(){
+window.onload = function () {
+    var url = 'https://api.spotify.com/v1/search?q=cats&type=album';
+    makeRequest(url, function () {
+        if (this.status !== 200) return;
+        var jsonString = this.responseText;
+        var results = JSON.parse(jsonString);
+        populateResults(results)
 
+      });
+  }  
+
+var makeRequest = function (url, callback) {
+    var request = new XMLHttpRequest();
+    request.open('GET', url);
+    request.onload = callback;
+    request.send();
 }
 
-window.onload = app;
+var populateResults = function(results){
+  console.log(results);
+}
